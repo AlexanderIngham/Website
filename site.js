704abd0dfa408660047e133da9334f4a7064cf0f
Alex Ingham <alexingham3@gmail.com>
2022-02-12 19:33:07 -0400

    Created about page and modified navbars

--- site.js
+++ site.js
@@ -3,5 +3,17 @@ function logoHover(element) {
 }
   
 function logoUnhover(element) {
-element.setAttribute('src', 'Images/Logo.png');
+    element.setAttribute('src', 'Images/Logo.png');
 }
+
+//Start css animations when the user's mouse enters the page
+$("#body-home").one("mouseover", function() {
+    document.getElementById("text").style.opacity = "1";
+    $("#text").addClass('textPermaHover');
+
+    document.getElementById("cone").style.opacity = "1";
+    $("#cone").addClass('conePermaHover');
+
+    $("#smallDot").addClass('smallDotPermaHover');
+    $("#bigDot").addClass('bigDotPermaHover');
+});
