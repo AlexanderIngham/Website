d6574593a724d816fe64b3c7822b671156612451
Alex Ingham <alexingham3@gmail.com>
2022-02-14 11:28:59 -0400

    Started About page

--- site.js
+++ site.js
@@ -6,23 +6,4 @@ function logoUnhover(element) {
     element.setAttribute('src', 'Images/Logo.png');
 }
 
-//Start css animations when the user's mouse enters the page
-$("#body-home").one("mouseover", function() {
-    document.getElementById("text").style.opacity = "1";
-    $("#text").addClass('textPermaHover');
 
-    document.getElementById("cone").style.opacity = "1";
-    $("#cone").addClass('conePermaHover');
-
-    $("#smallDot").addClass('smallDotPermaHover');
-    $("#bigDot").addClass('bigDotPermaHover');
-});
-
-var leftBrace = document.getElementById("leftBrace");
-var rightBrace = document.getElementById("rightBrace");
-document.addEventListener('mousemove', function(e){
-    var x = e.clientX;
-    var y = e.clientY;
-    leftBrace.style.transform = "translateX("+y/10+"px)";
-    rightBrace.style.transform = "translateX(-"+y/10+"px)";
-})
