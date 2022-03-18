8beae3db684960f5862945f2e6ed3d708e962124
Alex Ingham <alexingham3@gmail.com>
2022-03-18 15:52:53 -0300

    started fading animations

--- site.js
+++ site.js
@@ -6,4 +6,30 @@ function logoUnhover(element) {
     element.setAttribute('src', 'Images/Logo.png');
 }
 
+const faders = document.querySelectorAll
+('.fadeIn');
+
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -20px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, 
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
 
